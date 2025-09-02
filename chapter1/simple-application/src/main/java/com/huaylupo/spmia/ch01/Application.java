266e9532b52a057d20c1755cf47418076af378fb
household-four <barringerchristen4@gmail.com>
2025-09-01 19:58:46 -0700

    assignment 1.1

--- chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
+++ chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
@@ -12,42 +12,53 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(value="hello")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping(value="/{firstName}")
-	public String helloGET( 
+	@GetMapping(value = "/hello/{firstName}")
+	public String helloGET(
 			@PathVariable("firstName") String firstName,
 			@RequestParam("lastName") String lastName) {
-		return String.format("{\"message\":\"Hello %s %s\"}",firstName, lastName);
+		return String.format("{\"message\":\"Hello %s %s!\"}", firstName, lastName);
 	}
-	
-	@PostMapping
-	public String helloPOST( @RequestBody HelloRequest request) {
-		return String.format("{\"message\":\"Hello %s %s\"}",request.getFirstName(), request.getLastName());
+
+	@GetMapping(value = "/ping", params = "!name")
+	public String pingGET() {
+		return "pong";
+	}
+
+	@GetMapping(value = "/ping", params = "name")
+	public String pingGETName(@RequestParam("name") String name) {
+		return String.format("Pong %s ", name);
+	}
+
+	@PostMapping(value = "hello")
+	public String helloPOST(@RequestBody HelloRequest request) {
+		return String.format("{\"message\":\"Hello %s %s\"}", request.getFirstName(), request.getLastName());
 	}
 }
 
-class HelloRequest{
-	
+class HelloRequest {
+
 	private String firstName;
 	private String lastName;
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 }
-
---
+++ notes.md
@@ -0,0 +1,13 @@
+# How To
+
+## Build
+`cd` to the directory containing the pom.xml, then run: 
+
+`mvn clean verify`
+
+## Run/Test
+`cd` to the directory containing the pom.xml, then run: 
+
+`mvn spring-boot:run`
+
+Then navigate to localhost:8080./
